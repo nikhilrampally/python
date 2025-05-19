a99f0d844deb91d8ddcac948ebcc20b74aed96ec
nikhilrampally <sainikhil2494@gmail.com>
2025-05-19 13:37:32 +0000

    flask commit

---
+++ github-jira.py
@@ -0,0 +1,63 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route that handles GET requests
+@app.route('/createJira', methods=['POST'])
+def createJira():
+
+    url = "https://sainikhil2494.atlassian.net/rest/api/3/issue"
+
+    API_TOKEN= os.getenv("API_TOKEN")
+
+    auth = HTTPBasicAuth("sainikhil2494@gmail.com", API_TOKEN)
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+        "fields": {
+        "description": {
+            "content": [
+                {
+                    "content": [
+                        {
+                            "text": "Order entry fails when selecting supplier.",
+                            "type": "text"
+                        }
+                    ],
+                    "type": "paragraph"
+                    }
+                ],
+            "type": "doc",
+             "version": 1
+        },
+        "project": {
+           "key": "NIK"
+        },
+        "issuetype": {
+            "id": "10007"
+        },
+        "summary": "Main order flow broken",
+    },
+    "update": {}
+    } )
+
+
+    response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+    )
+
+    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
